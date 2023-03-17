537a82f8fefdcdb0dfa377194f09cf1124d671bf
Yishak Kidanu <yisak04@gmail.com>
2023-03-17 13:49:17 +0300

    Nav Menu Component added

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import {StyleSheet, css} from 'aphrodite';
 import logo from '../assets/List button.png';
 import CloseButton from '../assets/close.png';
+import sarahLogo from '../assets/SarahL.jpg';
 
 function Navigation() {
     const [showMenu, setShowMenu] = useState(false)
@@ -16,12 +17,9 @@ function Navigation() {
 
 
   return (
-    <div className={css(style.menu)}>
-    <button className={css(style.ListButton)} onClick={handleMenuOpen}>
-        <img src={logo} alt="" height="15px" width="22px"/>
-    </button>
-    {showMenu && (
-        <nav >
+    <div>
+    {showMenu ? (
+        <nav className={css(style.menu)}>
             <button className={css(style.buttonClose)} onClick={handleMenuClose}>
             <img src={CloseButton} alt="" height="25px" width="25px"/>
         </button>
@@ -29,27 +27,35 @@ function Navigation() {
             <li className={css(style.listItem)}>Home</li>
             <li className={css(style.listItem)}>Shop</li>
             <li className={css(style.listItem)}>About</li>
-            <li className={css(style.listItem)}>Contact</li>
+            <li className={css(style.listItem)}>Contact Us</li>
           </ul>
         </nav>
-      )}
+      ) : (
+        <div>
+            <img className={css(style.sLogo)} src={sarahLogo} alt="" />
+            <button className={css(style.ListButton)} onClick={handleMenuOpen}>
+            <img src={logo} alt="" height="15px" width="22px"/>
+            </button>
+        </div>
+      )} 
     </div>
   )
 }
 
 const style = StyleSheet.create({
     menu: {
-        backgroundColor:'#fff',
+        backgroundColor:'hsla(120, 100%, 25%, 0.3)',
         height: '100vh'
     },
     listItem: {
     margin: '10px 0',
     padding: '10px',
     fontSize: '50px',
-    // backgroundColor: '#fff',
     cursor: 'pointer',
     transition: 'all 0.3s ease-in-out',
-    display: 'flex'
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
 },
     ulItems: {
         alignItems: 'center',
@@ -59,13 +65,13 @@ const style = StyleSheet.create({
     ListButton: {
         padding: '0px',
         margin: '110px 120px',
-        // backgroundColor: 'white',
         border: 'none',
         borderRadius: '4px',
         cursor: 'pointer',
         float: 'right',
   },
     CloseButton: {
+        display: 'flex',
         padding: '10px 20px',
         margin: '20px 20px',
         fontSize: '16px',
@@ -74,9 +80,16 @@ const style = StyleSheet.create({
         border: 'none',
         borderRadius: '4px',
         cursor: 'pointer',
-        // float: 'right',
-        // justifyContent: 'flex-end'
+        top: '10px',
+        right: '10px',
+        transform: 'translateY(-50%)',
+        position: 'fixed'
+    },
+    sLogo: {
+        padding: '20px 20px',
+        margin: '70px 40px'
     }
+
   
 })
 
