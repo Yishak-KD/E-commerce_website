b5a5d79e43882075c20a6f39fe82924f94e84cc2
Yishak Kidanu <yisak04@gmail.com>
2023-03-24 21:34:17 +0300

    Contact us Page

--- src/Components/ContactUs.jsx
+++ src/Components/ContactUs.jsx
@@ -1,118 +1,142 @@
-import React, {useState} from 'react'
-import { StyleSheet, css } from 'aphrodite'
-
+import React, { useState } from "react";
+import { StyleSheet, css } from "aphrodite";
 
 function ContactUs() {
   const [contactState, SetContactState] = useState({
-    name: '',
-    email: '',
-    phone: '',
-    message: ''
-  })
-  const [error, setError] = useState(null)
+    name: "",
+    email: "",
+    phone: "",
+    message: "",
+  });
+  const [error, setError] = useState(null);
 
-  const handleContactState = event => {
+  const handleContactState = (event) => {
     const value = event.target.value;
     SetContactState({
       ...contactState,
-      [event.target.name]: value
-    })
-  }
+      [event.target.name]: value,
+    });
+  };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    if (!contactState.name || !contactState.email || !contactState.phone || !contactState.message) {
-      setError("Please fill in all required fields.")
+    if (
+      !contactState.name ||
+      !contactState.email ||
+      !contactState.phone ||
+      !contactState.message
+    ) {
+      setError("Please fill in all required fields.");
     } else {
       setError(null);
-      console.log(contactState)
+      console.log(contactState);
     }
-  }
+  };
   return (
     <div className={css(styles.container)}>
-        <h2>CONTACT US HERE AT SARAH'S ART GALLERY</h2>
-        <form className={css(styles.form)} onSubmit={handleSubmit}>
-          <label className={css(styles.label)}>Name*
-            <input type="name" name="name" value={contactState.name} onChange={handleContactState} className={css(styles.input)} />
-          </label>
-          <br />
-          <label className={css(styles.label)}>Email*
-            <input type="email" name="email" value={contactState.email} onChange={handleContactState} className={css(styles.input)} />
-          </label>
-          <br />
-          <label className={css(styles.label)}>Phone*
-            <input type="number" name="phone" value={contactState.phone} onChange={handleContactState} className={css(styles.input)} />
-          </label>
-          <br />
-          <label className={css(styles.label)}>Message*
-            <textarea name='message' cols={10} rows={10} value={contactState.message} onChange={handleContactState} className={css(styles.textarea)}></textarea>
-          </label>
-          {error && <p className={css(styles.error)}>{error}</p>}
-          <button type='submit' className={css(styles.button)}>SEND</button>
-        </form>
+      <h2>CONTACT US HERE AT SARAH'S ART GALLERY</h2>
+      <form className={css(styles.form)} onSubmit={handleSubmit}>
+        <label className={css(styles.label)}>Name*</label>
+        <input
+          type="name"
+          name="name"
+          value={contactState.name}
+          onChange={handleContactState}
+          className={css(styles.input)}
+        />
+        <label className={css(styles.label)}>Email*</label>
+        <input
+          type="email"
+          name="email"
+          value={contactState.email}
+          onChange={handleContactState}
+          className={css(styles.input)}
+        />
+        <label className={css(styles.label)}>Phone*</label>
+        <input
+          type="number"
+          name="phone"
+          value={contactState.phone}
+          onChange={handleContactState}
+          className={css(styles.input)}
+        />
+        <label className={css(styles.label)}>Message*</label>
+        <textarea
+          name="message"
+          cols={10}
+          rows={10}
+          value={contactState.message}
+          onChange={handleContactState}
+          className={css(styles.textarea)}
+        ></textarea>
+        <br />
+        {error && <p className={css(styles.error)}>{error}</p>}
+        <button type="submit" className={css(styles.button)}>
+          SEND
+        </button>
+      </form>
     </div>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   form: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: '50%',
-    margin: '0 auto',
-    marginTop: '50px',
-    padding: '20px',
-    border: '1px solid #ccc',
-    borderRadius: '5px',
+    display: "grid",
+    gridTemplateColumns: '30% 70%',
+    gap: '20px',
+    width: "55%",
+    // margin: "0 6%",
+    padding: "30px",
+    borderRadius: "5px",
+    backgroundColor: 'transparent'
   },
   container: {
-    textAlign: 'center',
-    fontFamily: 'Arial, sans-serif',
+    textAlign: "center",
+    fontFamily: "Arial, sans-serif",
+    backgroundColor: 'beige'
   },
   label: {
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'flex-start',
-    width: '21%',
-    marginRight: '75%',
-    lineHeight: '1.6em'
+    display: "flex",
+    alignItems: "center",
+    textAlign: "right",
+    margin: '0 50px'
   },
   input: {
-    padding: '10px 150px',
-    // borderRadius: 5,
-    border: '1px solid black',
-    // width: '100%',
-    marginBottom: 10,
-    boxSizing: 'border-box',
-    // flexGrow: 1
+    backgroundColor: 'transparent',
+    padding: "10px",
+    border: "1px solid black",
+    // marginBottom: 10,
+    boxSizing: "border-box",
+    fontSize: '18px'
   },
   textarea: {
-    padding: '10px 150px',
-    // borderRadius: 5,
-    border: '1px solid black',
-    // width: '100%',
+    border: "1px solid black",
     height: 200,
-    resize: 'vertical',
+    resize: "vertical",
     marginBottom: 10,
-    boxSizing: 'border-box'
+    boxSizing: "border-box",
+    textAlign:'left',
+    fontSize: '18px',
+    backgroundColor: 'transparent'
   },
   button: {
-    backgroundColor: 'black',
-    color: 'white',
-    border: 'none',
-    padding: '10px 15px',
-    borderRadius: 5,
-    cursor: 'pointer',
-    transition: 'background-color 0.3s ease',
-    ':hover': {
-      backgroundColor: '#0077cc'
-    }
+    backgroundColor: "black",
+    color: "white",
+    border: "none",
+    padding: "10px 15px",
+    cursor: "pointer",
+    gridColumnStart: "2",
+    justifySelf: "start",
+    alignSelf: "center",
+    transition: "background-color 0.3s ease",
+    ":hover": {
+      backgroundColor: "#0077cc",
+    },
   },
   error: {
-    color: 'red'
-  }
-})
+    color: "red",
+    marginLeft: '-300px'
+  },
+});
 
 export default ContactUs;
